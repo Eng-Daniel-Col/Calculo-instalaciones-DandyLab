841782320044a154f9e6cb15b35ac4739c41740b
Eng-Daniel-Col <danielharaujomoran@gmail.com>
2026-09-14 20:01:04 -0500

    Add initial application configuration settings

---
+++ config/settings.py
@@ -0,0 +1,12 @@
+APP_NAME = "DandyLab Ingeniería"
+
+APP_VERSION = "0.1.0"
+
+COMPANY_NAME = "DandyLab"
+
+APP_DESCRIPTION = (
+    "Sistema para gestión de instalaciones industriales, "
+    "cálculos eléctricos y generación de informes técnicos."
+)
+
+DEFAULT_LANGUAGE = "es"
